86b04bee213fec7833a314862689363053582577
Matthew O'Connell <mxoconnell@gmail.com>
2015-11-17 21:32:32 -0800

    Button Wiring
    All buttons now go to each scene they should

---
+++ iRememberV3/Assets/changeScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeScene : MonoBehaviour {
+
+	// Use this for initialization
+	public void ChangeToScene(string sceneToChangeTo){
+		Application.LoadLevel (sceneToChangeTo);
+	}
+}
